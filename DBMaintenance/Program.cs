ecceddfff3c09aeca0ae1d9c14501b8d88ab17ec
Odinlevi <odinlevi@gmail.com>
2022-12-01 14:59:09 +0200

    Provide configuration to docker

--- DBMaintenance/Program.cs
+++ DBMaintenance/Program.cs
@@ -7,26 +7,26 @@ namespace DBMaintenance;
 
 internal static class Program
 {
-    private const string ConfigurationPath = "Resources\\configuration.json";
+    private const string ConfigurationPath = "resources/configuration.json";
 
     public static void Main(string[] args)
     {
         var directoryAbsolutePath = AppDomain.CurrentDomain.BaseDirectory;
 
         var configurationAbsolutePath = Path.Combine(
-            directoryAbsolutePath, "..\\..\\..", ConfigurationPath);
+            directoryAbsolutePath, ConfigurationPath);
 
-        var configurator = new Configurator(configurationAbsolutePath);
-        var configurationData = configurator.Load();
-
-        var maintenanceService = new MaintenanceService(configurationData.MySqlAddresses);
+         var configurator = new Configurator(configurationAbsolutePath);
+         var configurationData = configurator.Load();
         
-        var maintenanceRouter = new MaintenanceRouter(maintenanceService);
-            
-        var maintenanceListener = new AsyncListener(
-            configurationData.MaintenancePrefixes, 
-            maintenanceRouter) as IAsyncListener;
-        maintenanceListener.Schedule();
+         var maintenanceService = new MaintenanceService(configurationData.MySqlAddresses);
+        
+         var maintenanceRouter = new MaintenanceRouter(maintenanceService);
+             
+         var maintenanceListener = new AsyncListener(
+             configurationData.MaintenancePrefixes, 
+             maintenanceRouter) as IAsyncListener;
+         maintenanceListener.Schedule();
         
         Thread.Sleep(Timeout.InfiniteTimeSpan);
     }
